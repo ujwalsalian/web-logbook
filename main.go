99a7f01b25cfdbf7d9eec7f2820fa0a669eddadd
ujwalsalian <ujwalsalian1999@gmail.com>
2024-11-01 01:14:48 +0200

    Update main.go

--- main.go
+++ main.go
@@ -6,16 +6,17 @@ import (
     "os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "index.html") // Serve the HTML file
-}
-
 func main() {
+    // Set the port from the environment variable or use default
     port := os.Getenv("PORT")
     if port == "" {
         port = "4000" // Default port if not specified
     }
-    http.HandleFunc("/", handler)
+
+    // Serve static files from the "app" directory
+    fs := http.FileServer(http.Dir("./app"))
+    http.Handle("/", fs)
+
     log.Printf("Server is running on port %s...", port)
     log.Fatal(http.ListenAndServe(":"+port, nil))
 }
